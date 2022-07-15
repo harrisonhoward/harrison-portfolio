651c449863ed8ff464af1e2b7158de04d0a98454
Harrison Howard <harrison.howard00707@gmail.com>
2022-07-15 17:00:33 +1000

    Initial structure of code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [themeType, setThemeType] = useState("dark");
+    const theme = createTheme({});
+
+    const setDarkTheme = () => setThemeType("dark");
+    const setLightTheme = () => setThemeType("light");
+    const toggleTheme = () =>
+        setThemeType(themeType === "dark" ? "light" : "dark");
+
+    return <ThemeProvider theme={theme}></ThemeProvider>;
 }
 
 export default App;
