5345d7b7918f32419d64796e4bfabf08d904e43e
Harrison Howard <harrison.howard00707@gmail.com>
2022-07-16 15:13:27 +1000

    Add scroll background to navbar

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AppBar, Toolbar, Container } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 
 import NavButton from "./NavButton";
 import useEventListener from "../../../hooks/useEventListener";
@@ -11,29 +12,50 @@ import routes from "../../../data/routes";
  * @param {{ isMobile?: boolean }} props
  */
 function Navbar(props) {
-    const [hasScrolled, setScrolled] = useState(false);
+    const [scrolledAmount, setScrolledAmount] = useState(0);
+    useEffect(() => {
+        setScrolledAmount(document.scrollingElement.scrollTop);
+    }, []);
     useEventListener("scroll", (evt) => {
-        const scrollOffset = document.scrollingElement.scrollTop;
-        if (scrollOffset >= 1 && !hasScrolled) setScrolled(true);
-        else if (scrollOffset < 1 && hasScrolled) setScrolled(false);
+        setScrolledAmount(document.scrollingElement.scrollTop);
     });
 
     return (
-        <AppBar position="fixed" variant={hasScrolled && "scrolled"}>
-            <Toolbar>
-                {props.isMobile ? (
-                    <p>m</p>
-                ) : (
-                    <Container variant="nav">
-                        {routes.map((route, index) => (
-                            <NavButton key={index} to={route.path}>
-                                {route.name}
-                            </NavButton>
-                        ))}
-                    </Container>
-                )}
-            </Toolbar>
-        </AppBar>
+        <AnimatePresence>
+            <AppBar position="fixed">
+                <motion.div
+                    style={{
+                        position: "absolute",
+                        background: "#09082b",
+                        boxShadow:
+                            "0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)",
+                        width: "100vw",
+                        height: "90px",
+                    }}
+                    initial={{
+                        top: `${-92 + scrolledAmount / 2}px`,
+                    }}
+                    animate={{
+                        top: `${
+                            scrolledAmount < 184 ? -92 + scrolledAmount / 2 : 0
+                        }px`,
+                    }}
+                />
+                <Toolbar>
+                    {props.isMobile ? (
+                        <p>m</p>
+                    ) : (
+                        <Container variant="nav">
+                            {routes.map((route, index) => (
+                                <NavButton key={index} to={route.path}>
+                                    {route.name}
+                                </NavButton>
+                            ))}
+                        </Container>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </AnimatePresence>
     );
 }
 
