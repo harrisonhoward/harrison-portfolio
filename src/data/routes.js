3988e1e54d7798505c1c83381470562d9876476b
Harrison Howard <harrison.howard00707@gmail.com>
2022-07-17 11:24:01 +1000

    Fix work path having a capital

--- src/data/routes.js
+++ src/data/routes.js
@@ -5,7 +5,7 @@ const routes = [
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Projects", path: "/projects" },
-    { name: "Work", path: "/Work" },
+    { name: "Work", path: "/work" },
     { name: "Contact", path: "/contact" },
 ];
 export default routes;
