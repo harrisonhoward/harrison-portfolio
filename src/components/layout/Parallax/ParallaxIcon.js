cbfd990410aefc34d51c433f9a408b8e0317eed9
Harrison Howard <harrison.howard00707@gmail.com>
2022-07-17 19:47:45 +1000

    Implement final changes

--- src/components/layout/Parallax/ParallaxIcon.js
+++ src/components/layout/Parallax/ParallaxIcon.js
@@ -1,30 +1,47 @@
-import React, { useState } from "react";
-import { styled } from "@mui/material";
+import React from "react";
+import { motion } from "framer-motion";
+import useMousePosition from "@react-hook/mouse-position";
 import * as DevIcon from "devicons-react";
 
 /**
  *
  * @param {{ icon: keyof import("devicons-react"), top?: string | number,
  * left?: string | number, right?: string | number, bottom?: string | number,
- * width?: string | number, height?: string | number, sx?: import("@mui/material").SxProps }} props
+ * width?: string | number, height?: string | number, depth?: number
+ * sx?: import("@mui/material").SxProps }} props
  */
 function ParallaxIcon(props) {
     const { top, left, right, bottom, width, height } = props;
     const Component = DevIcon[props.icon || "Aarch64Original"];
-    const IconContainer = styled("div")({
-        position: "absolute",
-        width: width || "32px",
-        height: height || "32px",
-        top,
-        left,
-        right,
-        bottom,
-    });
+    const mouseCoords = useMousePosition(document.getElementById("root"));
 
     return (
-        <IconContainer>
+        <motion.div
+            style={{
+                position: "absolute",
+                width: width || "32px",
+                height: height || "32px",
+                //filter: "blur(1px)",
+                top,
+                left,
+                right,
+                bottom,
+            }}
+            animate={{
+                x:
+                    (mouseCoords.elementHeight / 2 - mouseCoords.clientX) /
+                    (props.depth || 15),
+                y:
+                    (mouseCoords.elementWidth / 2 - mouseCoords.clientY) /
+                    (props.depth || 15),
+            }}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+            }}
+        >
             <Component style={props.sx} />
-        </IconContainer>
+        </motion.div>
     );
 }
 
--- src/components/layout/Parallax/index.js
+++ src/components/layout/Parallax/index.js
@@ -1,51 +1,90 @@
 import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import useMousePosition from "@react-hook/mouse-position";
+import { AnimatePresence } from "framer-motion";
 import { styled } from "@mui/material";
 
 import ParallaxIcon from "./ParallaxIcon";
-import useEventListener from "../../../hooks/useEventListener";
 
-const PARALLAX_DEPTH = 20;
+/**
+ * @type {{ icon: keyof import("devicons-react"), top?: string | number,
+ * left?: string | number, right?: string | number, bottom?: string | number,
+ * depth?: number }[]}
+ */
+const PARALLAX_ITEMS = [
+    {
+        icon: "JavascriptOriginal",
+        top: "10%",
+        left: "10%",
+        depth: 15,
+    },
+    {
+        icon: "ReactOriginal",
+        top: "20%",
+        left: "25%",
+        depth: 20,
+    },
+    {
+        icon: "ReduxOriginal",
+        top: "25%",
+        left: "50%",
+        depth: 30,
+    },
+    {
+        icon: "Html5Original",
+        top: "60%",
+        left: "20%",
+        depth: 30,
+    },
+    {
+        icon: "Css3Original",
+        top: "75%",
+        left: "40%",
+        depth: 25,
+    },
+    {
+        icon: "PostgresqlOriginal",
+        top: "40%",
+        left: "80%",
+        depth: 60,
+    },
+    {
+        icon: "MongodbOriginal",
+        top: "15%",
+        left: "70%",
+        depth: 50,
+    },
+    {
+        icon: "GithubOriginal",
+        top: "80%",
+        left: "80%",
+        depth: 80,
+        sx: {
+            borderRadius: "10%",
+            background: "white",
+        },
+    },
+];
 
 function Parallax() {
-    const mouseCoords = useMousePosition(document.getElementById("root"), {
-        fps: 60,
+    const ParallaxContainer = styled("div")({
+        position: "fixed",
+        left: 0,
+        top: 0,
+        height: "100vh",
+        width: "100vw",
+        overflow: "hidden",
     });
 
     return (
         <AnimatePresence initial={false}>
-            <motion.div
-                style={{
-                    position: "fixed",
-                    left: 0,
-                    top: 0,
-                    height: "100vh",
-                    width: "100vw",
-                    overflow: "hidden",
-                }}
-                animate={{
-                    x:
-                        -(mouseCoords.elementHeight / 2 - mouseCoords.clientX) /
-                        PARALLAX_DEPTH,
-                    y:
-                        -(mouseCoords.elementWidth / 2 - mouseCoords.clientY) /
-                        PARALLAX_DEPTH,
-                }}
-                transition={{
-                    type: "spring",
-                    stiffness: 100,
-                }}
-            >
-                <ParallaxIcon
-                    icon="JavascriptOriginal"
-                    top="10%"
-                    left="10%"
-                    sx={{
-                        borderRadius: "20%",
-                    }}
-                />
-            </motion.div>
+            <ParallaxContainer>
+                {PARALLAX_ITEMS.map((item, index) => (
+                    <ParallaxIcon
+                        {...item}
+                        key={index}
+                        sx={item.sx || { borderRadius: "20%" }}
+                    />
+                ))}
+            </ParallaxContainer>
         </AnimatePresence>
     );
 }
