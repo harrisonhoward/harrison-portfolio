9800385b490131362e14f910bc1bb3897990de68
Harrison Howard <harrison.howard00707@gmail.com>
2022-07-24 12:49:03 +1000

    redirect data for home page

---
+++ src/data/redirects.js
@@ -0,0 +1,33 @@
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+    faGithub,
+    faTwitter,
+    faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default [
+    {
+        username: "harrison.howard00707@gmail.com",
+        link: "mailto:{username}",
+        label: "Email",
+        icon: faEnvelope,
+    },
+    {
+        username: "harrisonhoward",
+        link: "https://github.com/{username}",
+        label: "Github",
+        icon: faGithub,
+    },
+    {
+        username: "harrison-howard",
+        link: "https://www.linkedin.com/in/{username}",
+        label: "LinkedIn",
+        icon: faLinkedin,
+    },
+    {
+        username: "harrisonhowardd",
+        link: "https://twitter.com/{username}",
+        label: "Twitter",
+        icon: faTwitter,
+    },
+];
