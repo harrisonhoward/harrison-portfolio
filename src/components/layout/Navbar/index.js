5e69ac561e35a34bf6514de0b8b51bc845002aab
Harrison Howard <harrison.howard00707@gmail.com>
2022-07-28 21:18:21 +1000

    Fix tiny gap appearing on high momentum

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -52,15 +52,15 @@ function Navbar() {
                         boxShadow:
                             "0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)",
                         width: "575px",
-                        height: navGlobals.height + 20,
+                        height: navGlobals.height + 30,
                         left: "calc(50% - 575px / 2)",
                     }}
                     initial={false}
                     animate={{
                         top:
                             scrolledAmount === navGlobals.scrollLimit
-                                ? -20
-                                : -(navGlobals.height + 30),
+                                ? -30
+                                : -(navGlobals.height + 40),
                     }}
                     transition={{
                         type: "spring",
