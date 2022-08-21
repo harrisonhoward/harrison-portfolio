dc443aa1033141ee8245e8319420d7b82e2779ba
Harrison Howard <harrison.howard00707@gmail.com>
2022-08-21 15:55:24 +1000

    Add DateTooltip component to About page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Element } from "react-scroll";
 import { Box, Typography } from "@mui/material";
 
 import GlassCard from "../components/ui/GlassCard";
 import Container from "../styles/Container";
+import GroupBox from "../styles/GroupBox";
 import ProfileImage from "../features/About/styles/ProfileImage";
+import DateTooltip from "../components/ui/DateTooltip";
+
+const DOB = new Date(2002, 5, 6);
 
 function About() {
+    // Using year, month and day calculate my age
+    const age = useMemo(() => {
+        const today = new Date();
+        const age = today.getFullYear() - DOB.getFullYear();
+        const m = today.getMonth() - DOB.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < DOB.getDate()))
+            return age - 1;
+        return age;
+    }, []);
+
     return (
         <Element name="/about">
             <Container>
@@ -33,7 +47,24 @@ function About() {
                         overflow: "visible",
                     }}
                 >
-                    <Typography>Content</Typography>
+                    <GroupBox
+                        sx={{
+                            marginTop: "1rem",
+                            marginLeft: "1rem",
+                        }}
+                    >
+                        <Box>
+                            <Typography variant="body1">
+                                I am currently&nbsp;
+                                <DateTooltip
+                                    date={DOB}
+                                    dateFormat={"dddd, Do MMMM YYYY"}
+                                >
+                                    <span>{age} years old</span>
+                                </DateTooltip>
+                            </Typography>
+                        </Box>
+                    </GroupBox>
                 </GlassCard>
             </Container>
         </Element>
