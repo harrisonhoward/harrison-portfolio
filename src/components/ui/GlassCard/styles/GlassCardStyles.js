e7819a0e62d5edeb182decd126f5e4d94fcd6fae
Harrison Howard <harrison.howard00707@gmail.com>
2022-09-02 14:31:50 +1000

    Increase opacity to make text visibility better

--- src/components/ui/GlassCard/styles/GlassCardStyles.js
+++ src/components/ui/GlassCard/styles/GlassCardStyles.js
@@ -2,7 +2,7 @@ import { styled, Card } from "@mui/material";
 const GlassCardStyles = styled(Card)({
     display: "flex",
     flexDirection: "column",
-    background: "rgba(26, 26, 26, 0.3)",
+    background: "rgba(26, 26, 26, 0.4)",
     backdropFilter: "blur(5px)",
     border: "1px solid rgba(255, 255, 255, 0.1)",
 });
