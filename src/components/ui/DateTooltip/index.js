43c873f45b965bf776b1a2fc3cf5391ca987a994
Harrison Howard <harrison.howard00707@gmail.com>
2022-09-02 16:34:22 +1000

    Give tooltip content colour and fix issues with multiline text

--- src/components/ui/DateTooltip/index.js
+++ src/components/ui/DateTooltip/index.js
@@ -5,12 +5,16 @@ import Tooltip from "./styles/Tooltip";
 
 /**
  *
- * @param {{ children: string, date: Date, dateFormat?: string }} props
+ * @param {{ children: string, date: Date, dateFormat?: string, sx?: import("@mui/material").SxProps }} props
  */
 function DateTooltip(props) {
     const format = props.dateFormat || "dddd, Do MMMM YYYY hh:mm:ss a";
     return (
-        <Tooltip title={moment(props.date).format(format)} placement="top">
+        <Tooltip
+            title={moment(props.date).format(format)}
+            placement="top"
+            sx={props.sx}
+        >
             {props.children}
         </Tooltip>
     );
--- src/components/ui/DateTooltip/styles/Tooltip.js
+++ src/components/ui/DateTooltip/styles/Tooltip.js
@@ -1,12 +1,20 @@
 import { styled, Tooltip } from "@mui/material";
-const StyledTooltip = styled(Tooltip)({
-    background: "rgba(6, 6, 6, 0.15)",
-    backdropFilter: "blur(5px)",
+import Color from "color";
+const StyledTooltip = styled(Tooltip)(({ theme }) => ({
+    background: `rgba(${Color(theme.palette.secondary.dark)
+        .darken(0.4)
+        .rgb()
+        .array()
+        .join(", ")}, 0.3)`,
     borderRadius: "4px",
-    padding: "0rem 0.15rem",
+    padding: "2px 3px",
     transition: "background 0.2s ease-in-out",
     "&:hover": {
-        background: "rgba(6, 6, 6, 0.3)",
+        background: `rgba(${Color(theme.palette.secondary.dark)
+            .darken(0.4)
+            .rgb()
+            .array()
+            .join(", ")}, 0.5)`,
     },
-});
+}));
 export default StyledTooltip;
