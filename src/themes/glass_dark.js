0d469d4e26c41da2e97820fa2dfa7a05546c48f1
Harrison Howard <harrison.howard00707@gmail.com>
2022-09-24 11:06:21 +1000

    Glassify dialogs

--- src/themes/glass_dark.js
+++ src/themes/glass_dark.js
@@ -112,6 +112,17 @@ const glass_dark = {
                 },
             },
         },
+        MuiDialog: {
+            styleOverrides: {
+                paper: {
+                    display: "flex",
+                    flexDirection: "column",
+                    background: "rgba(26, 26, 26, 0.4)",
+                    backdropFilter: "blur(5px)",
+                    border: "1px solid rgba(255, 255, 255, 0.1)",
+                },
+            },
+        },
     },
 };
 
