1e08660c6ab5a64e29243542cf72c083fbe15aa0
Harrison Howard <harrison.howard00707@gmail.com>
2022-12-10 11:42:32 +1000

    Routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Entry point
 import App from "./App";
+
+// Resources
 import "./core/ClassNameGenerator";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
