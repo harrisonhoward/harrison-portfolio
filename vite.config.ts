e257a71eac8c3bc28099ed1853f9a5ba4621216a
Harrison Howard <harrison.howard00707@gmail.com>
2024-04-07 17:34:32 +1000

    Fix minification warning on vite build (improved build process)

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,28 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: (id) => {
-                    if (id.indexOf("node_modules") !== -1) {
-                        return "vendor";
+                    const fileNameRegex = /^.*\/(.*)\..*$/;
+
+                    // Split node_modules into vendors
+                    if (id.includes("node_modules")) {
+                        // dev-icons are massive in file size so we need to split the icons into individual chunks (8MB+ combined)
+                        if (id.includes("devicons") && id.includes("icons")) {
+                            const iconName = fileNameRegex.exec(id)?.[1];
+                            return `vendor-devicon-${iconName}`;
+                        } else {
+                            const packageName = id
+                                .toString()
+                                .split("node_modules/")[1]
+                                .split("/")[0]
+                                .toString();
+
+                            // We are now only going to split into separate chunks packages that are problematic with size
+                            if (packageName.includes("react-dom")) {
+                                return "vendor-react";
+                            }
+
+                            return "vendor";
+                        }
                     }
                 },
             },
