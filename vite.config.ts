89471feb24b4d50267eaea3eb1d27034faf1f559
Harrison Howard <harrison.howard00707@gmail.com>
2024-05-24 20:18:44 +1000

    Update vite and config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        open: "http://howardfamily.ddns.net:3000/",
+        port: 3000,
+        strictPort: true,
+    },
     build: {
         // Only warn when our build exceeds 1000kB
         chunkSizeWarningLimit: 1000,
